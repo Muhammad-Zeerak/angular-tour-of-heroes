025b738a2101cc8aef647e79c91fa30031027854
Muhammad-Zeerak <muhammadzeerakroots@gmail.com>
2022-12-15 11:48:29 +0500

    added new files

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,19 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 16, name: 'Valkyrie' },
+  { id: 12, name: 'IronMan' },
+  { id: 17, name: 'Thor' },
+  { id: 18, name: 'Dr. Strange' },
+  { id: 13, name: 'Captain America' },
+  { id: 14, name: 'Hulk' },
+  { id: 15, name: 'Spiderman' },
+  { id: 19, name: 'Black Panther' },
+  { id: 20, name: 'Vision' },
+  { id: 21, name: 'Scarlet Witch' },
+  { id: 22, name: 'Black Widow' },
+  { id: 23, name: 'Hawkeye' },
+  { id: 24, name: 'Falcon' },
+  { id: 25, name: 'War Machine' },
+  { id: 26, name: 'Antman' },
+];
